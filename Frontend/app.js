b3fa342fa6c800b940bf0da9d805daac2a9eea68
SergioCAUTCH <sergiocarlosalmazan@gmail.com>
2021-07-15 14:50:38 -0600

    ahora si se finalizo el landingpage

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,10 +1,13 @@
-window.addEventListener('load', function(){
-  new Glider(document.querySelector('.glider'), {
-      slidesToShow: 1,
-      dots: '.dots',
-      arrows: {
-        prev: '.glider-prev',
-        next: '.glider-next'
-      }
-  })
+let Planicie = document.getElementById('planicie');
+let carretera = document.getElementById('carretera');
+let montanas = document.getElementById('montanas');
+let montanaslejanas = document.getElementById('montanaslejanas');
+let moto = document.getElementById('moto');
+
+window,addEventListener('scroll', function(){
+  let value = window.scrollY;
+  carretera.style.top= value * 0.03 + '%';
+  montanas.style.top= value *0.03 + '%';
+  montanaslejanas.style.top= value * 0.05 + '%';
+  moto.style.top= value * 0.05 + '%';
 })
